f4c80e5769e9a94c38f5ca92a16066a5a84cc478
MarcoRamosN <mramosn@gmail.com>
2020-08-06 15:35:52 +0000

    Initial commit

---
+++ edad.py
@@ -0,0 +1,5 @@
+nombre = "Cristian"
+edad = 38
+print("soy", nombre, end = " ")
+print("y tengo", edad, "años")
+print("cumplidos")
---
+++ main.py
@@ -0,0 +1,16 @@
+
+llegada = 14.25
+salida = 17.50
+tarifa = 200
+precio = ((salida - llegada) * tarifa)
+#print(precio)
+print("Usted debe $", str(precio), "pesos")
+
+# end se utiliza para no cambiar de línea
+nombre = "Cristian"
+edad = 38
+print("Soy", nombre, end = " ")
+print("y tengo", edad, "años")
+print("cumplidos")
+
+print("Soy", nombre, "y tengo", edad, "años\ncumplidos")
